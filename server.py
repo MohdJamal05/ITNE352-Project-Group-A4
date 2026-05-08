7ee3db5a277f7f1960455cc9aa492aa26eb56831
laylakhalil177-hub <laylakhalil177@gmail.com>
2026-05-08 14:28:05 +0300

    change

--- server.py
+++ server.py
@@ -1,1 +1,19 @@
+import socket
 
+HOST = '127.0.0.1'
+PORT = 5000
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind((HOST, PORT))
+server.listen(1)
+print("Server listening on port 5000...")
+
+conn, addr = server.accept()
+print(f"Client connected from {addr}")
+
+msg = conn.recv(1024).decode()
+print(f"Received: {msg}")
+
+conn.sendall(b"Hello from server!")
+conn.close()
+server.close()

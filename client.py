fdc0d1f2c884ae813b416dfc7f23588b48420212
laylakhalil177-hub <laylakhalil177@gmail.com>
2026-05-08 14:52:09 +0300

    cha

--- client.py
+++ client.py
@@ -1,10 +1,7 @@
 import socket
 
-HOST = '127.0.0.1'
-PORT = 5000
-
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect((HOST, PORT))
+client.connect(('127.0.0.1', 12000))
 
 client.sendall(b"Hello from client!")
 

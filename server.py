734aea813354c6f1df3f36229a2b156fe7f9f591
laylakhalil177-hub <laylakhalil177@gmail.com>
2026-05-08 14:51:36 +0300

    change

--- server.py
+++ server.py
@@ -1,13 +1,10 @@
 import socket
 
-HOST = '127.0.0.1'
-PORT = 5000
-
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-server.bind((HOST, PORT))
+server.bind(('127.0.0.1', 12000))
 server.listen(1)
-print("Server listening on port 5000...")
+print("Server listening on port 12000...")
 
 conn, addr = server.accept()
 print(f"Client connected from {addr}")
